181c0cd9195d06c5c8b7d2fb96a50a46ed64babe
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-05 18:50:39 +0900

    meeting.model.js 수정

--- app/models/meeting.model.js
+++ app/models/meeting.model.js
@@ -1,16 +1,21 @@
 module.exports = (mongoose) => {
-  const Meeting = mongoose.model(
-    "meeting",
-    mongoose.Schema(
-      {
-        MeetingName: String,
-        MeetingDates: [Date],
-        startHour: String,
-        endHour: String,
-      },
-      { timestamps: true }
-    )
+  const schema = mongoose.Schema(
+    {
+      MeetingName: String,
+      MeetingDates: [Date],
+      startHour: String,
+      endHour: String,
+    },
+    { timestamps: true }
   );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Meeting = mongoose.model("meeting", schema);
 
   return Meeting;
 };
