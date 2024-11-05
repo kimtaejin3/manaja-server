9b6c2a87603d7369e53417a40e6ab26e305867d8
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-05 18:30:47 +0900

    cors등 미들웨어 적용

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8080;
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+// app.use(express.urlencoded({extended:true}))
+
 app.get("/", (req, res) => {
-  res.send("manaja server start!");
+  res.json({ message: "manaja server start!" });
 });
 
 app.listen(port, () => {
