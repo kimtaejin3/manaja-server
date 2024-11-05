94783cea93cf80289d2fca57676dfd1554524bfe
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-05 19:44:50 +0900

    mongodb 연결

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(express.urlencoded({extended:true}))
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 require("./app/routes/meeting.routes")(app);
 
 app.listen(port, () => {
